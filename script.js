d9dd6d4095246d95dd77e3acab337916eaa7de94
Anushka9061 <anushka.shinde9061@walchandsangli.ac.in>
2026-06-30 11:08:03 +0530

    Implement task management functions with localStorage

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function displayTasks(){
+    let list=document.getElementById("taskList");
+    list.innerHTML="";
+
+    tasks.forEach((task,index)=>{
+
+        let li=document.createElement("li");
+
+        li.innerHTML=`
+        <strong class="${task.completed?'completed':''}">
+        ${task.name}
+        </strong><br>
+        ${task.date}<br>
+
+        <div class="actions">
+            <button onclick="completeTask(${index})">✔</button>
+            <button onclick="editTask(${index})">Edit</button>
+            <button onclick="deleteTask(${index})">Delete</button>
+        </div>
+        `;
+
+        list.appendChild(li);
+
+    });
+
+}
+
+function addTask(){
+
+    let task=document.getElementById("taskInput").value;
+    let date=document.getElementById("taskDate").value;
+
+    if(task==""){
+        alert("Enter Task");
+        return;
+    }
+
+    tasks.push({
+        name:task,
+        date:date,
+        completed:false
+    });
+
+    saveTasks();
+    displayTasks();
+
+    document.getElementById("taskInput").value="";
+    document.getElementById("taskDate").value="";
+}
+
+function deleteTask(index){
+    tasks.splice(index,1);
+    saveTasks();
+    displayTasks();
+}
+
+function completeTask(index){
+    tasks[index].completed=!tasks[index].completed;
+    saveTasks();
+    displayTasks();
+}
+
+function editTask(index){
+
+    let newTask=prompt("Edit Task",tasks[index].name);
+
+    if(newTask){
+        tasks[index].name=newTask;
+        saveTasks();
+        displayTasks();
+    }
+
+}
+
+displayTasks();
